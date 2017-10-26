7bed867a838f6cc23d8489c977a0c6b65a12b823
Julian Alexander and Joe McKinney <joe@>
2017-10-25 20:10:51 -0700

    Added all conditional statements to JS and created divs to link to form, plus point system for the end character result

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//Back End
+var points = 0;
+
+
+
+
+
+//UI
+$(document).ready(function() {
+  $("#questions").submit(function(e) {
+    event.preventDefault();
+    var situation1 = $("#situation1").val();
+    var situation2 = $("#situation2").val();
+
+    if (situation1 === "1") {
+      points += 100
+    } else if (situation1 === "2") {
+      points += 50
+    } else if (situation1 === "3") {
+      points += 1
+    } else if (situation1 === "4") {
+      points += 500
+    } else if (situation1 === "0") {
+      alert("Hey, you didn't select anything. Try again!")
+      confirm("do you get it?")
+      alert("Good, now try again.")
+    }
+
+    if (situation2 === "1") {
+      points += 100
+    } else if (situation2 === "2") {
+      points += 1
+    } else if (situation2 === "3") {
+      points += 50
+    } else if (situation2 === "4") {
+      points += 500
+    } else if (situation1 ==="0") {
+      alert("Thanks Lil B")
+    }
+
+    if (points  < 3) {
+      $("#draco").show();
+    } else if (points > 4 && points < 101) {
+      $("#neville").show();
+    } else if (points > 101 && points <250) {
+      $("#bellatrix").show();
+    } else if (points >599) {
+      $("#mcgonagall").show();
+    }
+
+
+
+  });
+});
