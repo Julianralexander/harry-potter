c1c9b9b5ecb1d823248cae0419f856e3a9ad391e
Julian Alexander <julianlapis@gmail.com>
2017-10-29 20:09:36 -0700

    Added a bunch of formatting and readme info

---
+++ README.md
@@ -0,0 +1,29 @@
+# _{One Direction Quiz}_
+
+#### _{A small quiz that tells the user which one direction member they are.}_
+
+#### By _**{Julian Alexander}**_
+
+## Description
+
+_{I built this site for my second code review for Epicodus' evening class, using HTML, CSS, Javascript and Bootstrap + Jquery.}_
+
+## Setup/Installation Requirements
+
+* _Extract the Git Repo_
+* _Clone into your home directory_
+* _Make sure the Bootstrap and Jquery links are correct and up to date_
+* _Run and Enjoy_
+
+
+## Known Bugs
+
+_{When you return results on the quiz that don't follow the linear points method (ie trying to go back to 200 if you already have 500) it won't adjust your result, but it will adjust it if you ADD points instead.}_
+
+## Support and contact details
+
+_{Any concerns, or bugs can be reported to the creator at - julianlapis@gmail.com.}_
+
+## Technologies Used
+
+_{Javascript and Jquery.}_
--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ var points = 0;
 
 
 
-
 //UI
 $(document).ready(function() {
   $("#questions").submit(function(e) {
@@ -21,9 +20,8 @@ $(document).ready(function() {
     } else if (situation1 === "4") {
       points += 500
     } else if (situation1 === "0") {
-      alert("Hey, you didn't select anything. Try again!")
-      confirm("do you get it?")
-      alert("Good, now try again.")
+      alert("Hey, you didn't select anything...Try again!")
+      confirm("Did you do it?")
     }
 
     if (situation2 === "1") {
@@ -39,25 +37,25 @@ $(document).ready(function() {
     }
 
     if (points  < 3 && points >1) {
-      $("#bellatrix").hide();
-      $("#mcgonagall").hide();
-      $("#neville").hide();
-      $("#draco").show();
+      $("#niall").show();
+      $("#harry").hide();
+      $("#zayn").hide();
+      $("#liam").hide();
     } else if (points > 4 && points < 101) {
-      $("#bellatrix").hide();
-      $("#mcgonagall").hide();
-      $("#draco").hide();
-      $("#neville").show();
+      $("#liam").show();
+      $("#harry").hide();
+      $("#zayn").hide();
+      $("#niall").hide();
     } else if (points > 101 && points <250) {
-      $("#draco").hide();
-      $("#mcgonagall").hide();
-      $("#neville").hide();
-      $("#bellatrix").show();
+      $("#harry").show();
+      $("#zayn").hide();
+      $("#niall").hide();
+      $("#liam").hide();
     } else if (points >599) {
-      $("#bellatrix").hide();
-      $("#draco").hide();
-      $("#neville").hide();
-      $("#mcgonagall").show();
+      $("#zayn").show();
+      $("#harry").hide();
+      $("#liam").hide();
+      $("#niall").hide();
     }
 
 
